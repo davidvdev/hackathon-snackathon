496e81fc6a976c5249bdc834f8f633a90f8420df
David Vogel <david@davidvdev.com>
2021-11-16 14:30:44 -0600

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 ReactDOM.render(
-  <Router>
     <React.StrictMode>
-      <Route path="/" component={App}/>
-    </React.StrictMode>
-  </Router>,
+      <App />
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
