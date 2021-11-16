5a2be97b7ce96c8bc4a8dc9509579b917d45483d
seemabmahajan <seemabmahajan@gmail.com>
2021-11-16 12:56:35 -0800

    Results page added

---
+++ src/Pages/Results.js
@@ -0,0 +1,48 @@
+import React from 'react'
+//import Navbar from 'Components/Navbar.js'
+
+const Results = (props) => {
+    return ( 
+    <div>
+        <Navbar/> 
+        <img src={props.results.img} /> 
+        <h1>{props.results.name}</h1>
+        //filter choices
+        <h2>Ingredients</h2>
+        <div className='recepie'>
+            <ul>
+            {props.results.ingredients.map((item, index) => {
+              
+              return (
+                  
+                      <li>{item}</li> 
+                      
+                 )
+              
+           })}
+          </ul>
+        </div>
+
+        <h2>Directions</h2>
+
+        <div className=''>
+            <ul>
+            {props.results.directions.map((item, index) => {
+              
+              return (
+                  
+                      <li>{item}</li> 
+                      
+                 )
+              
+           })}
+          </ul>
+        </div>
+
+
+
+    </div>
+
+    )
+}
+export default Results
