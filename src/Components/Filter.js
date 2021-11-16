7bf1d5d71216c53e49cfdc132b250bfc285d29c1
davidvdev <david@davidvdev.com>
2021-11-16 17:09:31 -0600

    basic structure of filter pages set

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
-const Filter = (props) => {
-    return ( <div>
-        <button >
-            Props Filter name will go here!
-        </button>
-    </div>
-
+const Filter = ({ handleClick, key, keyValues}) => {
+    return ( 
+        <div className="filter">
+            <div className="options">
+                {/* buttons are placeholders for card component */}
+                <button onClick={() => handleClick(key, keyValues[0])}>{keyValues[0]}</button>
+                <button onClick={() => handleClick(key, keyValues[1])}>{keyValues[1]}</button>
+                <button onClick={() => handleClick(key, keyValues[2])}>{keyValues[2]}</button>
+                <button onClick={() => handleClick(key, keyValues[3])}>{keyValues[3]}</button>
+            </div>
+        </div>
     )
 }
 export default Filter
--- src/Pages/Filters.js
+++ src/Pages/Filters.js
@@ -3,10 +3,48 @@ import Filter from "../components/Filter"
 
 const Filters = (props) => {
 
+    const emptyChoices = {
+        holiday: "",
+        meal: "",
+        course: "",
+        ingredients: "",
+        time: "",
+        temp: ""
+    }
+
     const [progress, setProgress] = useState(0)
+    const [choices, setChoices] = useState(emptyChoices)
+
+    const next = (key, value) => {
+        setChoices({...choices, [key]: value})
+    }
+
+    const keyValues= {
+        holiday: ["Christmas", "New Years", "Easter", "Thanksgiving"],
+        meal: [],
+        course: [],
+        ingredients: [],
+        time: [],
+        temp: []
+    }
 
     return(
-        <Filter />
+        <div className="Filters">
+            <h2>Ready for a {props.holidy || "Merry Christmas"}</h2>
+            <div className="progressBar">{progress}</div>
+            <Filter handleClick={next} keyValues={keyValues.holiday} key={"holiday"}/>
+            <div className="changeQuestion">
+            {progress == 0 &&
+                <button onClick={() => setProgress(progress + 1)}>Next Question</button>
+            }
+            {progress > 0 &&
+            <>
+                <button onClick={() => setProgress(progress - 1)}>Previous Question</button>
+                <button onClick={() => setProgress(progress + 1)}>Next Question</button>
+            </>
+            }
+            </div>
+        </div>
     )
 }
 
