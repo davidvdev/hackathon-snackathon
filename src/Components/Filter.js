0399d794db5c20df0ef151239eb323800f6fcb00
davidvdev <david@davidvdev.com>
2021-11-16 17:11:26 -0600

    added state to Filter for highlighting selected

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Filter = ({ handleClick, key, keyValues}) => {
+
+    const [selected, setSelected] = useState("")
+
     return ( 
         <div className="filter">
             <div className="options">
