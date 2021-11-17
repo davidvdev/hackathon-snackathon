e359c6ace34bbf17ce8133f94e13e8e9a36ee001
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-11-17 09:20:44 -0500

    navbar edit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const Navbar = (props) => {
     return ( 
         <div>
-            <button><Link to="/App">Home</Link></button>
+            <button><Link to="/">Home</Link></button>
             <h1>Reci-Please</h1>
         </div>
     )
