fc35db0d634fcb7f4169714371cb01c11348bb9c
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-11-17 15:02:25 -0500

    landing page styles.

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import React, { useState, useEffect } from 'react'
+import MyCard from '../MyCard'
 
 const Buttons = (props) => {
     
@@ -19,22 +20,29 @@ const Buttons = (props) => {
     }
     
     return (
-                <div>
-                    {randomRecipe !== 0 &&
-                    <>
-                        <h1>{randomRecipe.obj.name}</h1>
-                        <h2>{randomRecipe.obj.description}</h2>
-                        <h3>{randomRecipe.obj.recipeingredientparts}</h3>
-                        <h4>{randomRecipe.obj.recipeingredientquantities}</h4>
-                    </>
-                    }
-                
         <div>
+            {randomRecipe !== 0 &&
+                <>
+                    <h1>{randomRecipe.obj.name}</h1>
+                    <h2>{randomRecipe.obj.description}</h2>
+                    <h3>{randomRecipe.obj.recipeingredientparts}</h3>
+                    <h4>{randomRecipe.obj.recipeingredientquantities}</h4>
+                </>
+            }
+            <div className='buttons'>
+                <div className='button'>
+                <h2>Click for a Random Recipe</h2>
+                <img src=''/>
+                    <button onClick={() => randomButton(props)}>Random Recipe</button>
                 </div>
-            <button onClick={() => randomButton(props)}>Search Random Recipe</button>
-            <Link to='/filters'>
-                <button>Search With Filters?</button>
-            </Link>
+                <div className='button'>  
+                    <h2>Search for a Recipe</h2>
+                    <img src=''/>
+                    <Link to='/filters'>
+                        <button>Find a Recipe</button>
+                    </Link>  
+                </div>
+            </div>          
         </div>
     )
 }
